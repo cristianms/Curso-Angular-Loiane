101e9873e00c9e3022e55f65e91b6a9a70b31f50
Cristian de Mellos <cristian.ms@hotmail.com>
2019-07-11 08:12:23 -0300

    Conhecendo Two Way Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import { CursosModule } from './cursos/cursos.module';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormComponent } from './meu-form/meu-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiro2Component,
-    DataBindingComponent
+    DataBindingComponent,
+    MeuFormComponent
   ],
   imports: [
     BrowserModule,
-    CursosModule
+    CursosModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
